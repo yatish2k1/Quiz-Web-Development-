fbcefbbad5dcded9bfb5e6df34b2da38348569cf
Sudreesha <sudreeshacapri@gmail.com>
2021-11-11 10:14:37 +0530

    quiz

---
+++ js/questions.js
@@ -0,0 +1,121 @@
+let questions=[
+    {
+        num:1,
+        question:'The robot is?',
+        answer: 'All of the above',
+        options:[
+            'Is very primitive at the present',
+            'Represents the most powerful new industrial automation',
+            'Will be more useful when robot version is improved',
+            'All of the above'
+        ]
+    },
+
+    {
+        num:2,
+        question:'The end effector of a robot is',
+        answer: 'All of the above',
+        options:[
+            'Can to actual tool',
+            'Many have a gripping action',
+            'Is a robot hand',
+            'All of the above'
+        ]
+    },
+
+    {
+        num:3,
+        question:'Robotic manipulator are mostly used',
+        answer:'Electric motor actuator',
+        options:[
+            'Hydraulic actuator',
+            'Pneumatic actuator',
+            'Electric motor actuator',
+            'None of the above',
+        ]
+    },
+
+    {
+        num:4,
+        question:'End effector is also called as',
+        answer:'Grippers',
+        options:[
+            'Gripper',
+            'Tool',
+            'actuator',
+            'joints',
+        ]
+    },
+
+    {
+        num:5,
+        question:'The drive which uses fluid',
+        answer:'Hydraulic drive system',
+        options:[
+            'Electric drive system',
+            'Hydraulic drive system',
+            'Pneumatic drive system',
+            'None of the above',
+        ]
+    },
+
+    {
+        num:6,
+        question:'Telescopic arm is used in',
+        answer:'Polar configuration',
+        options:[
+            'Jointed arm configuration',
+            'Cartesian configuration',
+            'Polar configuration',
+            'Cylindrical cinfiguration',
+        ]
+    },
+
+    {
+        num:7,
+        question:'Which of the following is not an advantage of robotics',
+        answer:'Time consuming',
+        options:[
+            'Efficient',
+            'No fatigue',
+            'Speed',
+            'Time consuming',
+        ]
+    },
+
+    {
+        num:8,
+        question:'Which type of gripper used for ferrous objects',
+        answer:'Magnetic Gripper',
+        options:[
+            'Vaccum Gripper',
+            'Mechanical Gripper',
+            'Magnetic Gripper',
+            'Adhesive Gripper',
+        ]
+    },
+
+    {
+        num:9,
+        question:'Which type of Gripper uses negative air pressure',
+        answer:'Vaccum Gripper',
+        options:[
+            'Vaccum Gripper',
+            'Mechanical Gripper',
+            'Magnetic Gripper',
+            'Adhesive Gripper',
+        ]
+    },
+
+    {
+        num:10,
+        question:'Which of following can be used as a sensor in robot',
+        answer:'Thermometer',
+        options:[
+            'brake',
+            'clutch',
+            'Resistor',
+            'Thermometer',
+        ]
+    },
+];
